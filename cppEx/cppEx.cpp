3dae509bfed473265423ef3a1105f1addbc3e668
hemh <maoqin.kx@gmail.com>
2020-05-16 10:57:17 +0800

    [add] 字符编码转换函数

--- cppEx/cppEx.cpp
+++ cppEx/cppEx.cpp
@@ -20,3 +20,38 @@
 //{
 //    return;
 //}
+
+CPPEX_API wchar_t* NP_CPPEX::AnsiToUnicode(const char* szStr)
+{
+	int nLen = MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, szStr, -1, NULL, 0);
+	if (nLen == 0)
+		return NULL;
+
+	wchar_t* pResult = new wchar_t[nLen] {};
+	MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, szStr, -1, pResult, nLen);
+	return pResult;
+}
+
+CPPEX_API char* NP_CPPEX::UnicodeToAnsi(const wchar_t* szStr)
+{
+	int nLen = WideCharToMultiByte(CP_ACP, 0, szStr, -1, NULL, 0, NULL, NULL);
+	if (nLen == 0)
+		return NULL;
+	
+	char* pResult = new char[nLen] {};
+	WideCharToMultiByte(CP_ACP, 0, szStr, -1, pResult, nLen, NULL, NULL);
+	return pResult;
+}
+
+CPPEX_API bool NP_CPPEX::IsPath(const char* szStr)
+{
+	std::tr2::sys::path tmpPath;
+
+	return false;
+}
+
+CPPEX_API bool NP_CPPEX::IsFile(const char* szStr)
+{
+	std::tr2::sys::path tmpPath;
+	return false;
+}
--- cppEx/cppEx.h
+++ cppEx/cppEx.h
@@ -1,5 +1,5 @@
 #include "ICppEx.h"
-
+#include <filesystem>
 
 
 //// 此类是从 cppEx.dll 导出的
@@ -16,5 +16,11 @@
 
 namespace NP_CPPEX
 {
+	// 字符转换
+	CPPEX_API inline wchar_t* AnsiToUnicode(const char* szStr);
+	CPPEX_API inline char* UnicodeToAnsi(const wchar_t* szStr);
 
+	// 文件路径
+	CPPEX_API bool IsPath(const char* szStr);
+	CPPEX_API bool IsFile(const char* szStr);
 }
