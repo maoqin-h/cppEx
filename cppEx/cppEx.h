e57a84a13f671ad513e394fe754533d28ee74068
hemh <maoqin.kx@gmail.com>
2020-06-23 13:24:46 +0800

    [add] 支持C调用, 添加版本信息

--- cppEx/cppEx.h
+++ cppEx/cppEx.h
@@ -7,47 +7,54 @@
 #define CPPEX_API __declspec(dllimport)
 #endif
 
+
 // 开启 TOOLLOG
 //#define ENABLE_TOOLLOG
 
+#if defined __cplusplus
+extern "C" {
+#endif 
 
-namespace NP_CPPEX
-{
-	// 字符转换
-	CPPEX_API inline wchar_t*  AnsiToUnicode(const char* szStr);
-	CPPEX_API inline char*  UnicodeToAnsi(const wchar_t* szStr);
-
-	// 文件路径
-	CPPEX_API bool  IsPath(const char* szStr);
-	CPPEX_API bool  IsFile(const char* szStr);
-
-	class CPPEX_API CMyToolLog
+	namespace NP_CPPEX
 	{
-	public:
-		static CMyToolLog& GetInstance();
-		static void LOGINFO(const char* szLoginfo);
-		static void LOGWARNING(const char* szLogWarning);
-		static void LOGERROR(const char* szLogError);
+		// 字符转换
+		CPPEX_API inline wchar_t*  AnsiToUnicode(const char* szStr);
+		CPPEX_API inline char*  UnicodeToAnsi(const wchar_t* szStr);
+
+		// 文件路径
+		CPPEX_API bool  IsPath(const char* szStr);
+		CPPEX_API bool  IsFile(const char* szStr);
+
+		class CPPEX_API CMyToolLog
+		{
+		public:
+			static CMyToolLog& GetInstance();
+			static void LOGINFO(const char* szLoginfo);
+			static void LOGWARNING(const char* szLogWarning);
+			static void LOGERROR(const char* szLogError);
 
 
-	protected:
-		CMyToolLog();
-		virtual ~CMyToolLog();
+		protected:
+			CMyToolLog();
+			virtual ~CMyToolLog();
 
-	protected:
-		void CreateConsoleLog(const char* chLogfilepath = nullptr);
-		void DestroyConsoleLog();
+		protected:
+			void CreateConsoleLog(const char* chLogfilepath = nullptr);
+			void DestroyConsoleLog();
 
-	private:
-		bool	m_bConsole;			// 已创建console
-		FILE*	m_pIn;
-		FILE*	m_pOut;
-		FILE*	m_pErr;
-	};
-	
+		private:
+			bool	m_bConsole;			// 已创建console
+			FILE*	m_pIn;
+			FILE*	m_pOut;
+			FILE*	m_pErr;
+		};
 
+
+	}
+
+#if defined __cplusplus
 }
-
+#endif 
 
 #ifdef ENABLE_TOOLLOG
 #define TOOL_LOGINSTANCE NP_CPPEX::CMyToolLog::GetInstance();
